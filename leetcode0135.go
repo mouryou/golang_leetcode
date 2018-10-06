427d3a23f48f6d75a9e1a2920f3469ceec5420c1
mouryou <shengtax@andrew.cmu.edu>
2018-10-06 00:03:33 -0500

    improve 135

--- leetcode0135.go
+++ leetcode0135.go
@@ -1,54 +1,31 @@
 func candy(ratings []int) int {
-    start, peak, res, l := 0, 0, 0, len(ratings)
-    increasing, padding := true, false
-    for i := 1; i < l; i++ {
+    start, peak, res, padding, l := 0, 0, 0, 0, len(ratings)
+    ratings = append(ratings, ratings[l - 1])
+    increasing := true
+    for i := 1; i <= l; i++ {
         cur, prev := ratings[i], ratings[i - 1]
         if cur == prev || (!increasing && cur > prev) {
             if ratings[i - 1] > ratings[start] && start == peak {
                 peak = i - 1
             }
             a, b := peak - start, i - 1 - peak
-            if padding {
-                if a + 1 > b {
-                    a++
-                } else {
-                    b++
-                }
-                res += (2 + a + 1) * a / 2 + (1 + b) * b / 2
+            if a + padding > b {
+                a++
             } else {
-                if a > b {
-                    a++
-                } else {
-                    b++
-                }
-                res += (1 + a) * a / 2 + (1 + b) * b / 2
+                b++
             }
+            res += (1 + padding + a + padding) * a / 2 + (1 + b) * b / 2
             start, peak = i, i
-            padding = cur > prev
+            if cur > prev {
+                padding = 1
+            } else {
+                padding = 0
+            }
             increasing = true
         } else if increasing && cur < prev {
             peak = i - 1
             increasing = false
         } 
-    }
-    if ratings[l - 1] > ratings[start] && start == peak {
-        peak = l - 1
-    }
-    a, b := peak - start, l - 1 - peak
-    if padding {
-        if a + 1 > b {
-            a++
-        } else {
-            b++
-        }
-        res += (2 + a + 1) * a / 2 + (1 + b) * b / 2
-    } else {
-        if a > b {
-            a++
-        } else {
-            b++
-        }
-        res += (1 + a) * a / 2 + (1 + b) * b / 2
     }
     return res
 }
